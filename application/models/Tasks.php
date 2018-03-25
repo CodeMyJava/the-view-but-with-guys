6bad25b052c9881324ec12949515d6a136d9d7cc
rostgoat <rdavidoff@hotmail.ca>
2018-03-25 14:42:06 -0700

    store function override

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -34,6 +34,21 @@ public function load()
   $this->reindex();
 }
 
+public function store()
+{
+  // rebuild the keys table
+  $this->reindex();
+
+  if (($handle = fopen($this->_origin, "w")) !== FALSE)
+  {
+    fputcsv($handle, $this->_fields);
+    foreach ($this->_data as $key => $record)
+      fputcsv($handle, array_values((array) $record));
+    fclose($handle);
+  }
+
+}
+
   function getCategorizedTasks()
 {
     // extract the undone tasks
